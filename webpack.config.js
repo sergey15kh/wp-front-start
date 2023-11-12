8002f33f809fb44678aa5582e5f7a3f31e93476d
Serhii Aliev <62428651+sergey15kh@users.noreply.github.com>
2023-11-12 11:18:41 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/script.js',
+    entry: './assets/js/script.js',
     output: {
         filename: 'global.js',
         path: path.resolve(__dirname, './assets/js/'),
